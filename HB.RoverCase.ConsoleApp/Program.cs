4e46bf4afb6c20230daaec64b4f6f691c2c49311
Hasan Gök <hasangok@gmail.com>
2020-08-22 20:17:08 +0300

    Created project structure

---
+++ HB.RoverCase.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HB.RoverCase.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
