2357fe6a5d039a77b4ca2965a19c31a290cf4b41
Hasan Gök <hasangok@gmail.com>
2020-08-23 21:32:39 +0300

    Added command to check if Rover goes beyond plateau boundries

---
+++ HB.RoverCase.Models/Command/CheckBoundriesCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HB.RoverCase.Models
+{
+    public class CheckBoundriesCommand : ICommand
+    {
+        private IRover Rover { get; set; }
+        public CheckBoundriesCommand(IRover rover)
+        {
+            Rover = rover;
+        }
+        public void Execute()
+        {
+            var location = Rover.GetLocation();
+            var plateau = Rover.GetPlateau();
+            if(location.GetX() > plateau.GetX() || location.GetX() < 0 || location.GetY() > plateau.GetY() || location.GetY() < 0)
+            {
+                Console.WriteLine("Movement outside of plateau boundries!");
+            }
+        }
+    }
+}
