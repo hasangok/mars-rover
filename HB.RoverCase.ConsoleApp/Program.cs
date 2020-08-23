f00cc7070cdf24efabed14d1221e7dc6fa3a38bf
Hasan Gök <hasangok@gmail.com>
2020-08-23 21:51:13 +0300

    Mars Rover problem is implemented on console app

--- HB.RoverCase.ConsoleApp/Program.cs
+++ HB.RoverCase.ConsoleApp/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.IO;
+using HB.RoverCase.Models;
+using HB.RoverCase.Extensions;
 
 namespace HB.RoverCase.ConsoleApp
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] command = File.ReadAllLines("command.txt");
+
+            string plateauInput = command[0];
+            Plateau plateau = new Plateau(plateauInput.GetX(), plateauInput.GetY());
+
+            for (int i = 1; i <= command.Length - 1; i = i + 2)
+            {
+                try
+                {
+                    var initCommand = command[i];
+                    var movementCommand = command[i + 1];
+                    Rover r = new Rover(new Location(initCommand.GetX(), initCommand.GetY(), initCommand.GetHeading()), plateau);
+                    var commands = movementCommand.GetCommandsFromInputString(r);
+                    r.SetCommands(commands);
+                    r.ExecuteCommands();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
         }
     }
 }
